7b85f8317a87dc36db711e1b2a96e6cf5858a431
Jean Justen <jean.cjm@hotmail.com>
2025-04-24 22:59:09 -0300

    fixed bubbles on different browsers

--- src/scripts/bubble.js
+++ src/scripts/bubble.js
@@ -3,6 +3,13 @@ const bubbles = document.querySelectorAll(
     ".bubble1, .bubble2, .bubble3, .bubble4, .bubble5, .bubble6, .bubble7, .bubble8, .bubble9, .bubble10"
 );
 
+// Detect user's browser
+const isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+const isSafari = /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+
+// Plays the sound only if its not Chrome or Safari
+const playSound = !(isChrome || isSafari);
+
 // SFX List
 const popSounds = [
     new Audio("build/dist/audio/pop1.mp3"),
@@ -12,21 +19,23 @@ const popSounds = [
 
 bubbles.forEach(function(bubble) {
     bubble.addEventListener("click", function() {
-        // Choosing a sound randomly
-        const randomPop = popSounds[Math.floor(Math.random() * popSounds.length)];
-        const clonedSound = randomPop.cloneNode(); // Allow multiple SFXs to play at once
-        clonedSound.play();
+        // SFX if allowed
+        if (playSound) {
+            const randomPop = popSounds[Math.floor(Math.random() * popSounds.length)];
+            const clonedSound = randomPop.cloneNode();
+            clonedSound.play();
+        }
 
-        // Pops Bubble
+        // Pops the bubble
         bubble.classList.add("explode");
 
-        // Bubble becomes invisible and with no interaction
+        // Vanish Bubble
         setTimeout(() => {
             bubble.style.opacity = "0";
             bubble.style.pointerEvents = "none";
         }, 1000);
 
-        // Bubble respawns slowly
+        // Respawn Bubble
         setTimeout(() => {
             bubble.classList.remove("explode");
             bubble.style.opacity = "0.9";
